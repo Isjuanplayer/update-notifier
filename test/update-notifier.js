a44099d7d32c156f162ad29c60480375dd0a2142
Isjuanplayer <180686912+Isjuanplayer@users.noreply.github.com>
2026-05-11 14:24:47 +0200

    Add update notifier cache tests

--- test/update-notifier.js
+++ test/update-notifier.js
@@ -5,8 +5,8 @@ import esmock from 'esmock';
 
 const generateSettings = (options = {}) => ({
 	pkg: {
-		name: 'update-notifier-tester',
-		version: '0.0.2',
+		name: options.name ?? 'update-notifier-tester',
+		version: options.version ?? '0.0.2',
 	},
 	distTag: options.distTag,
 });
@@ -45,6 +45,49 @@ test('fetch info with dist-tag', async t => {
 	configstorePath = updateNotifier(generateSettings()).config.path;
 	const update = await updateNotifier(generateSettings({distTag: '0.0.3-rc1'})).fetchInfo();
 	t.is(update.latest, '0.0.3-rc1');
+});
+
+test('loads cached update once and clears it', async t => {
+	const updateNotifier = await esmock('../index.js', undefined, {'is-in-ci': false});
+	const notifier = updateNotifier(generateSettings({name: 'update-notifier-cache-test'}));
+	configstorePath = notifier.config.path;
+
+	notifier.config.set('update', {
+		latest: '1.0.0',
+		current: '0.0.1',
+		type: 'major',
+		name: 'update-notifier-cache-test',
+	});
+
+	notifier.check();
+
+	t.deepEqual(notifier.update, {
+		latest: '1.0.0',
+		current: '0.0.2',
+		type: 'major',
+		name: 'update-notifier-cache-test',
+	});
+	t.is(notifier.config.get('update'), undefined);
+});
+
+test('does not load cached update when opted out', async t => {
+	const updateNotifier = await esmock('../index.js', undefined, {'is-in-ci': false});
+	const notifier = updateNotifier(generateSettings({name: 'update-notifier-opt-out-test'}));
+	configstorePath = notifier.config.path;
+
+	const update = {
+		latest: '1.0.0',
+		current: '0.0.1',
+		type: 'major',
+		name: 'update-notifier-opt-out-test',
+	};
+
+	notifier.config.set('optOut', true);
+	notifier.config.set('update', update);
+	notifier.check();
+
+	t.is(notifier.update, undefined);
+	t.deepEqual(notifier.config.get('update'), update);
 });
 
 test('don\'t initialize configStore when NO_UPDATE_NOTIFIER is set', async t => {
